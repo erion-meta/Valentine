1104d4c51f69f6080de4ab8e84d966cadef0877b
erion-meta <metaerion79@gmail.com>
2024-02-13 22:13:36 +0100

    New commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesPage").addEventListener("click", function () {
+  window.location.href = "html/page1.html";
+});
+
+document.getElementById("noPage").addEventListener("click", function () {
+  window.location.href = "html/page2.html";
+});
